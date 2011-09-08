876d631809558652c9e5559c8e90d2aa719ab4a0
Ingo Schommer <ingo@silverstripe.com>
2011-09-08 14:37:12 +0200

    ENHANCEMENT Supporting SS_Cache in OpenSearchDescription->load()

--- code/model/OpenSearchDescription.php
+++ code/model/OpenSearchDescription.php
@@ -22,6 +22,11 @@ class OpenSearchDescription {
 	protected $xml = null;
 	
 	/**
+	 * @var SS_Cache
+	 */
+	protected $cache;
+	
+	/**
 	 * @param String $url Absolute URL to a valid OpenSearch description document.
 	 */
 	function __construct($url) {
@@ -32,22 +37,36 @@ class OpenSearchDescription {
 	 * @param String $url
 	 */
 	function load() {
-		// TODO Caching
-		$reqClass = (class_exists('SS_HTTPRequest')) ? 'SS_HTTPRequest' : 'OpenSearchHTTPRequest';
-		$c = Object::create('OpenSearchHTTPClient');
-		$response = $c->request(new $reqClass('GET', $this->url));
-		if($response->getStatusCode() >= 400) throw new Exception(sprintf('Invalid description (Code: %d, Response: %s)', $response->getStatusCode(), $response->getBody()));
-		$this->xml = simplexml_load_string($response->getBody());
+		$cache = $this->getCache();
+		$cacheKey = sha1($this->url);
 		
-		if(!$this->xml->Url || !count($this->xml->Url)) {
-			throw new Exception('No valid URLs in description document');
-		}
-		
-		foreach($this->xml->Url as $url) {
-			$this->urls[] = array(
-				'template' => (string)$url["template"],
-				'type' => (string)$url["type"],
-			);
+		if($xml = $cache->load($cacheKey)) {
+			$this->xml = simplexml_load_string($xml);
+		} else {
+			$reqClass = (class_exists('SS_HTTPRequest')) ? 'SS_HTTPRequest' : 'OpenSearchHTTPRequest';
+			$c = Object::create('OpenSearchHTTPClient');
+			$response = $c->request(new $reqClass('GET', $this->url));
+			if($response->getStatusCode() >= 400) throw new Exception(sprintf('Invalid description (Code: %d, Response: %s)', $response->getStatusCode(), $response->getBody()));
+			$xmlStr = $response->getBody();
+			$this->xml = simplexml_load_string($xmlStr);
+			var_dump($this->url);
+			var_dump($this->xml->Url);
+			var_dump(count($this->xml->Url));
+			if(!$this->xml->Url || !count($this->xml->Url)) {
+				throw new Exception(sprintf(
+					'No valid URLs in description document at %s',
+					$this->url
+				));
+			}
+
+			foreach($this->xml->Url as $url) {
+				$this->urls[] = array(
+					'template' => (string)$url["template"],
+					'type' => (string)$url["type"],
+				);
+			}
+			
+			$cache->save($xmlStr);
 		}
 	}
 	
@@ -87,6 +106,18 @@ class OpenSearchDescription {
 		if(!$this->xml) $this->load();
 		
 		return (string)$this->xml->Description;
+	}
+	
+	function setCache($cache) {
+		$this->cache = $cache;
+	}
+	
+	function getCache() {
+		if(!$this->cache) {
+			$this->cache = SS_Cache::factory('opensearch_descriptions');
+			SS_Cache::set_cache_lifetime('opensearch_descriptions', 60*60 /* one hour */, 100);
+		}
+		return $this->cache;
 	}
 	
 }
